d9c92ac773308c67779a2ccf7732dfa2a67d1c29
dimostant <dimarasgv@gmail.com>
2020-05-21 20:25:18 +0300

    added comments to the code about its use

--- MainNode/main/main.ino
+++ MainNode/main/main.ino
@@ -1,8 +1,8 @@
-#include "freertos/FreeRTOS.h"
+//#include "freertos/FreeRTOS.h" exists in esp_event.h
 #include "esp_wifi.h"
 #include "esp_wifi_types.h"
 #include "esp_system.h"
-#include "esp_event.h"
+//#include "esp_event.h" exists in esp_event.h
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
@@ -22,21 +22,21 @@ static void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t 
   printf("CHAN=%02d, RSSI=%02d\n",ppkt->rx_ctrl.channel,ppkt->rx_ctrl.rssi);
 }
 
-static esp_err_t event_handler(void *ctx, system_event_t *event){return ESP_OK;}
+static esp_err_t event_handler(void *ctx, system_event_t *event){return ESP_OK;}//it handles events but how does it fail?
 
 static void wifi_sniffer_init(void){
-  
   nvs_flash_init();
   tcpip_adapter_init();
-  ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+  ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );//runs a loop of handling events...needs more research
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+  ESP_ERROR_CHECK( esp_wifi_init(&cfg) );//checks if wifi init has Allocated resource for WiFi driver & has started the wifi task successfully
   ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) ); /* set country for channel range [1, 13] */
-  ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
-  ESP_ERROR_CHECK( esp_wifi_start() );
+  ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );//checks if WiFi API is set successfully to store to ram.// should do WIFI_STORAGE_FLASH for saving in ram and sd(memory and flash)
+  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );//checks if WiFi is set successfully to null mode.
+  ESP_ERROR_CHECK( esp_wifi_start() );//checks if wifi can be started according to data we set
   esp_wifi_set_promiscuous(true);//enables promiscuous mode (meaning that receiver also analyzes data of packets instead of only receiving and tranfering them 
   esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);//whenever a package is received , promiscuous data are given to the parameter function 
+}
 
 void setup() {
   Serial.begin(115200);
