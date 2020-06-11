940292c7cdd69ddb1a436cad2a6e5035bd3f2409
DELMARKOS <markos.delaportas@gmail.com>
2020-06-11 23:51:16 +0300

    embedded systems development sucks

--- MainNode/Neo_6M_code/Neo_6M_code.ino
+++ MainNode/Neo_6M_code/Neo_6M_code.ino
@@ -1,43 +1,26 @@
-#define NEO6M_RX_PIN D2 // board D2 --> neo6m TX
-#define NEO6M_TX_PIN D3 // board D3 --> neo6m RX
+#define NEO6M_RX_PIN 1 // board TX --> neo6m RX
+#define NEO6M_TX_PIN 3 // board RX --> neo6m TX
 
-#include <TinyGPS++>
+#define GPSBAUD 9600          //this is the serial baud rate that will be used for the GPS, a common default
 
-static TinyGPSPlus gpsParser;
-static TinyGPSPlus emptyGpsParser;
-static SoftwareSerial gpsSerial(NEO6M_RX_PIN, NEO6M_TX_PIN);
+#define GPSTX 3              //pin number for TX output from Arduino - RX into GPS
+#define GPSRX 1              //pin number for RX input into Arduino - TX from GPS
 
-void Neo6m::setup()
+#define GPSserial Serial2     //define GPSserial as ESP32 Serial2 
+
+void setup()
 {
-    Serial.print("Setting up Neo-6M GPS module... ");
-    gpsSerial.begin(NEO6M_BAUD);
-    Serial.println("done");
+  
+  GPSserial.begin(9600, SERIAL_8N1, GPSTX, GPSRX);           //format is baud, mode, UART RX data, UART TX data 
+                                                                 
+  Serial.begin(115200);
+  Serial.println("26_GPS_Echo_ESP32 Starting");
 }
 
-TinyGPSPlus Neo6m::read()
+void loop()
 {
-    while (gpsSerial.available() > 0)
-        if (gpsParser.encode(gpsSerial.read()))
-            return gpsParser;
-
-    return emptyGpsParser;
-}
-
-void Neo6m::print(TinyGPSPlus gpsParser)
-{
-    if (gpsParser.location.isValid())
-    {
-        Serial.print("Latitude: ");
-        Serial.println(gpsParser.location.lat(), 6);
-        Serial.print("Longitude: ");
-        Serial.println(gpsParser.location.lng(), 6);
-        Serial.print("Altitude: ");
-        Serial.println(gpsParser.altitude.meters());
-    }
-    else
-    {
-        Serial.println("Location: Not Available");
-    }
-
-    ...
+  while (GPSserial.available())
+  {
+    Serial.write(GPSserial.read());
+  }
 }
