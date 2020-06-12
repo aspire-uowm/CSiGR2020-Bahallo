bc49eca6d1c3e6bc696a55c5e9ce1376a1e16940
DELMARKOS <markos.delaportas@gmail.com>
2020-06-12 21:16:26 +0300

    Added GPS Support
    partially working

--- MainNode/Neo_6M_code/Neo_6M_code.ino
+++ MainNode/Neo_6M_code/Neo_6M_code.ino
@@ -1,24 +1,19 @@
-#define NEO6M_RX_PIN 1 // board TX --> neo6m RX
-#define NEO6M_TX_PIN 3 // board RX --> neo6m TX
-
 #define GPSBAUD 9600          //this is the serial baud rate that will be used for the GPS, a common default
 
 #define GPSTX 3              //pin number for TX output from Arduino - RX into GPS
 #define GPSRX 1              //pin number for RX input into Arduino - TX from GPS
 
-#define GPSserial Serial2     //define GPSserial as ESP32 Serial2 
+#define GPSserial Serial2    //define GPSserial as ESP32 Serial2 
 
-void setup()
-{
+void setup(){
   
-  GPSserial.begin(9600, SERIAL_8N1, GPSTX, GPSRX);           //format is baud, mode, UART RX data, UART TX data 
+  GPSserial.begin(9600, SERIAL_8N1, GPSTX, GPSRX);//format is baud, mode, UART RX data, UART TX data 
                                                                  
   Serial.begin(115200);
   Serial.println("26_GPS_Echo_ESP32 Starting");
 }
 
-void loop()
-{
+void loop(){
   while (GPSserial.available())
   {
     Serial.write(GPSserial.read());
