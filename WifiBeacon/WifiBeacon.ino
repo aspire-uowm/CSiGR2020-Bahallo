83796f617d39049707f7fcc7f871b6a881a7f9ea
DELMARKOS <markos.delaportas@gmail.com>
2020-06-17 12:25:06 +0300

    Made changes to working tree

--- WifiBeacon/WifiBeacon.ino
+++ WifiBeacon/WifiBeacon.ino
@@ -1,17 +1,15 @@
-//Code to be inserted into LoRa for receiving Udp packages:
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
 const char* ssid = "PacketReceiver"; // wifi ssid
 const char* password = "ekalagamhsou"; //wifi password
 
-const char* udpAddress = "192.168.43.204"; //destination IP
+const char* udpAddress = "192.168.43.155"; //destination IP
 WiFiUDP udp;
 unsigned int udpPort=1234; //Server port
 
 char pktbuf[10];//buffer to store udp data
-char rx_val; //*not sure yet*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+char rx_val;
 void setup() 
 {
   Serial.begin(115200);
@@ -22,9 +20,9 @@ void setup()
   {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
-    ESP.restart();
+    ESP.restart(); 
   }
-
+  
   //after Connection is established:
   Serial.println(WiFi.localIP()); //prints wifi ip to console
   Serial.println("Status: Connected");
@@ -32,16 +30,15 @@ void setup()
   //Enable udp
   udp.begin(udpPort);
   Serial.println(udpPort);
+
 }
 
 void loop() 
 {
-  int rp = udp.parsePacket(); //checks for udp packets
-  
-  //while we dont have a packet:
-  if(!rp)
+  int rp1=udp.parsePacket();
+  if(!rp1)
   {
-    //if lora is occupied:
+     //if lora is occupied:
     if(Serial.available() > 0) 
     {
       rx_val = Serial.read(); //not sure!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -50,13 +47,6 @@ void loop()
       udp.beginPacket(udpAddress, udpPort);
       udp.write(rx_val);
       udp.endPacket();
-    }
-    else
-    {
-      udp.read(pktbuf, 1);
-      Serial.print("Packet from " + String(udpAddress) + ": ");
-      Serial.println(pktbuf);
-      delay(1000); 
-    } 
   }
+
 }
