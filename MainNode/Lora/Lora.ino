afa2c62720a779a34c57c517c3236ea30905e92d
DELMARKOS <markos.delaportas@gmail.com>
2020-06-18 15:23:05 +0300

    LoRa mwe

--- MainNode/Lora/Lora.ino
+++ MainNode/Lora/Lora.ino
@@ -1,8 +1,6 @@
-/*//Libraries for LoRa
+#include <LoRa.h>
 #include <SPI.h>
-#include <LoRa.h>
 
-//define the pins used by the LoRa transceiver module
 #define SCK 5
 #define MISO 19
 #define MOSI 27
@@ -12,67 +10,25 @@
 
 //866E6 for Europe
 #define BAND 866E6
-
-int GPSBaud = 9600;
-
-void setup() {
-  
-  //Setup Lora
-  //initialize Serial Monitor
-  Serial.begin(115200);
-  Serial.println("LoRa Sender Test");
-
-  //SPI LoRa pins
-  SPI.begin(SCK, MISO, MOSI, SS);
-  
-  //Setup LoRa transceiver module
-  LoRa.setPins(SS, RST, DIO0);
-  
-  //Test Lora if Begins
-  while (!LoRa.begin(BAND)) 
-  {
-    Serial.println("Starting LoRa failed!");
-  }
-  Serial.println("LoRa Initializing OK!");
-
-  delay(2000);
-}
-
-void loop() 
-{
-    Lora.beginPacket();
-    if (gps.location.isValid()){
-      Lora.print(gps.location.lat(), 6);
-    }
-
-}*/
-
-
-//...........................................Some other code
-
-#include <LoRa.h>
-#define SCK 5
-#define MISO 19
-#define MOSI 27
-#define SS 18
-#define RST 14
-#define DIO0 26
-
-//866E6 for Europe
-#define BAND 866E6
-
 
 void setup() {
   // put your setup code here, to run once: 
   Serial.begin(115200);
   Serial.println("LoRa Receiver starting");
 
-  LoRa.setPins(18,14,26);
+  //LoRa.setPins(18,14,26);
 
-  if(!LoRa.begin(BAND)){
+  //SPI LoRa pins
+  //SPI.begin(SCK, MISO, MOSI, SS);
+  SPI.begin();
+  
+  //Setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+
+  while(!LoRa.begin(BAND)){
       Serial.println("Could not find a valid LoRa transceiver, check pins used and wiring!");
       delay(100);  
-  }
+  }Serial.print("All good, ready!!");
 }
 
 void loop() {
@@ -85,5 +41,4 @@ void loop() {
   LoRa.endPacket();
 
   LoRa.sleep();
-
 }
