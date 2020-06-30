016dc7ef4427f848df9af345d002e1c9fc5859a8
DELMARKOS <markos.delaportas@gmail.com>
2020-06-30 22:30:15 +0300

    Removed unnecessary code

--- Beacon/Udp.h
+++ Beacon/Udp.h
@@ -1,26 +1,51 @@
-#include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-#ifdef __cplusplus
-  extern "C"{
-#endif
+static WiFiUDP udp;
 
-    char* Udpssid = "Node"; // wifi ssid
-    char* Udppassword = '\0'; //wifi password
+char* Udpssid = "Node"; // wifi ssid
+char* Udppassword = '\0'; //wifi password
 
-    char* udpAddress = "192.168.43.155"; //destination IP
-    unsigned int udpPort=1234; //Server port
+char* udpAddress = "192.168.43.155"; //destination IP
+unsigned int udpPort=1234; //Server port
 
-    byte mac[] = { 0xFE, 0xBB, 0x34, 0xE5, 0x29, 0xFC };
+byte mac[] = { 0xFE, 0xBB, 0x34, 0xE5, 0x29, 0xFC };
 
-    WiFiUDP udp;
+char pktbuf[10];//buffer to store udp data
+char rx_val;
 
-    char pktbuf[10];//buffer to store udp data
-    char rx_val;
-
-    void UdpInit();
-    void UdpRun();
-#ifdef __cplusplus
+void UdpInit() {
+  WiFi.begin(Udpssid,Udppassword);
+  
+  //things that happen until Wifi Connection is established:
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    //ESP.restart(); 
   }
-#endif
+  
+  //after Connection is established:
+  Serial.println(WiFi.localIP()); //prints wifi ip to console
+  Serial.println("Status: Connected");
+
+  //Enable udp
+  udp.begin(udpPort);
+  Serial.println(udpPort);
+
+}
+
+void UdpRun() { 
+  int rp1=udp.parsePacket();
+  if(!rp1){
+     //if lora is occupied:
+    if(Serial.available() > 0) 
+    {
+      rx_val = Serial.read(); //not sure!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      Serial.print("udp_send: ");
+      Serial.println(rx_val);
+      udp.beginPacket(udpAddress, udpPort);
+      udp.write(rx_val);
+      udp.endPacket();
+    }
+  }
+}
