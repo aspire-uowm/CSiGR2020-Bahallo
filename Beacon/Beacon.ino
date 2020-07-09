9c74cde5b6aeb8e5df0c399779ae392fdb4059fc
DELMARKOS <markos.delaportas@gmail.com>
2020-07-09 05:04:45 +0300

    Packed up the code a bit
    just so only the pros can understand

--- Beacon/Beacon.ino
+++ Beacon/Beacon.ino
@@ -6,9 +6,10 @@
 #define motorPin 2
 #define runMotor(from,msecs,to) servo.write(from);delay(msecs);servo.write(to)
 
-#define rdAlt 1010  /*!!!!! Adjusted to local forecast !!!!! */
-int flag = 0, height_lock = 800 , height_brake = 70;
+#define rdAlt 1010  /* Adjusted to local forecast ! */
+#define Height 70
 
+bool flag = false;
   
 Servo servo; 
 
@@ -23,7 +24,7 @@ void setup()
 
   CheckandConnect(NodeNames[0]);
 
-  //runMotor(0,200,90); 
+  runMotor(0,200,90); 
 }
 
 void loop() 
@@ -31,25 +32,8 @@ void loop()
   //for(int i=0;i<3; i++)Check_WiFi_and_Connect(NodeNames[i]);
   //Send_Data_To_Server();
 
-  if(i == 0)                  //Check if Servo just started
-  {
-    delay(5000); 
-    runMotor(90,500,0);       //Servo Turns in 90
-    flag = 1;                 //Servo Lock
-  }
+  if((bmp.readAltitude(rdAlt) < 70) && flag) runMotor(90,500,0);
+  else flag = !(bmp.readAltitude(rdAlt) > 70) && flag;
   
-  if (bmp.readAltitude(rdAlt) > ( /*height*/ + height_lock))
-  {
-    flag = 2;                 //Servo Armed  
-  }
-  
-  if(bmp.readAltitude(rdAlt) <= ( /*height*/ + height_brake) )
-  {
-    if(flag == 2)             //Check if Servo is Armed
-    {
-      runMotor(90,500,0);     //Servo Turns in 0
-      delay(5000);            
-    }
-  }
   printServer("Hello!\r");
 }
